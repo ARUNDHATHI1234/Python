541c187ccf75f45000ee418e2cc465480d06df5f
mparu <mparundhathi@gmail.com>
2021-03-22 22:12:20 +0530

    commit msg1

---
+++ co3/co3_program3.py
@@ -0,0 +1,12 @@
+from co3.area import rectangle as r
+from co3.area import circle as c
+from co3.area.sec_pack import cuboid as cu, sphere as s
+
+print (c.area(2))
+print(c.peri(2))
+print(r.area(2,2))
+print(r.perim(2,2))
+print(cu.area(2,2,2))
+print(cu.perim(2,2,2,))
+print(s.area(2))
+print(s.perim(2))
